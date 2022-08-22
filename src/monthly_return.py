2027d9482f842e39975ee66ce2fc861f5c6d796a
wiz <wilsonzhou92@gmail.com>
2022-08-22 17:05:41 +0200

    adding comments

--- src/monthly_return.py
+++ src/monthly_return.py
@@ -1,6 +1,12 @@
 from sympy import symbols, Eq, solve
 
 def get_monthly_return(annual_return: float):
+    """
+    Based on an annual stock return, this function
+    calculates the average monthly return based on
+    solving r in the following formula:
+        (1+r)^1 + (1+r)^2 +...+ (1+r)^12 = 1 + r
+    """
     
     r = symbols('r', positive = True)
     lhs_eq = sum((1 + r) ** i for i in range(1, 12 + 1)) / 12
